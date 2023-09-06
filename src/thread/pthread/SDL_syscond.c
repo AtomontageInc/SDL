7681854880bda12934ab9da594f56083f354ac76
Matúš Fedorko <mat.fedorko@gmail.com>
2023-09-06 19:42:01 +0200

    Fix mutex ownership in pthread SDL_CondWait()

--- src/thread/pthread/SDL_syscond.c
+++ src/thread/pthread/SDL_syscond.c
@@ -133,6 +133,10 @@ tryagain:
     default:
         retval = SDL_SetError("pthread_cond_timedwait() failed");
     }
+#if FAKE_RECURSIVE_MUTEX
+    mutex->owner = pthread_self();
+    mutex->recursive = 0;
+#endif
     return retval;
 }
 
@@ -146,6 +150,10 @@ int SDL_CondWait(SDL_cond *cond, SDL_mutex *mutex)
     } else if (pthread_cond_wait(&cond->cond, &mutex->id) != 0) {
         return SDL_SetError("pthread_cond_wait() failed");
     }
+#if FAKE_RECURSIVE_MUTEX
+    mutex->owner = pthread_self();
+    mutex->recursive = 0;
+#endif
     return 0;
 }
 
